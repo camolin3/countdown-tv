f96c29c4bc0ce0882c4a874062eb216fb5587ff4
Carlos Molina A <ing.cmolina@gmail.com>
2018-06-12 00:18:33 -0400

    First version.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, Subject, interval } from 'rxjs';
+import { finalize, map, startWith, take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  counter$: Observable<string>;
+  stop$ = new Subject();
+  running = false;
+  /**
+   * Time, in seconds, of the current countdown.
+   */
+  time = 90;
+
+  start() {
+    const format = n => {
+      const floor = Math.floor(n);
+      return (floor < 10) ? '0' + floor : String(floor);
+    };
+    this.running = true;
+    const el = document.getElementById('countdown');
+    el.mozRequestFullScreen();
+    this.counter$ = interval(1000).pipe(
+      startWith(-1),
+      map(s => this.time - s - 1),
+      map(s => [format(s / 3600), format(s / 60), format(s % 60)].join(':')),
+      take(1 + this.time),
+      takeUntil(this.stop$),
+      finalize(() => this.running = false),
+    );
+  }
+
+  stop() {
+    document.mozCancelFullScreen();
+    this.stop$.next();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
