2d19625d32ef444dfd11e29ca5018dc31e4a6af4
Carlos Molina A <cmolina@kunder.cl>
2018-06-20 12:36:48 -0400

    Font is loaded async. WIP: Pause. Input handle '1h 30m 10s' strings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable, Subject, interval } from 'rxjs';
-import { finalize, map, startWith, take, takeUntil } from 'rxjs/operators';
+import { Observable, Subject, interval, never, BehaviorSubject } from 'rxjs';
+import { finalize, map, startWith, take, takeUntil, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +9,18 @@ import { finalize, map, startWith, take, takeUntil } from 'rxjs/operators';
 })
 export class AppComponent {
   counter$: Observable<{ hh: string[], mm: string[], ss: string[] }>;
+  paused$ = new BehaviorSubject<boolean>(false);
   stop$ = new Subject();
   running = false;
+  timeStr = '1h 30m';
   /**
    * Time, in seconds, of the current countdown.
    */
-  time = 90;
+  get time() {
+    const [, h, m, s] = this.timeStr.match(/(\d*\.?\d*h\s*)?(\d*\.?\d*m\s*)?(\d*\.?\d*s\s*)?/);
+    const strToNumber = (str) => Number((str || '0').match(/\d*\.?\d+/)[0]);
+    return 3600 * strToNumber(h) + 60 * strToNumber(m) + strToNumber(s);
+  }
 
   start() {
     const format = n => {
@@ -25,11 +31,15 @@ export class AppComponent {
     this.running = true;
     /* const el = document.getElementById('countdown');
     el.mozRequestFullScreen(); */
-    this.counter$ = interval(1000).pipe(
+    const numbers$ = interval(1000).pipe(
       startWith(-1),
       map(s => this.time - s - 1),
-      map(s => ({ hh: format(s / 3600), mm: format(s / 60), ss: format(s % 60) })),
       take(1 + this.time),
+      finalize(() => console.log('why?')),
+    );
+    this.counter$ = this.paused$.pipe(
+      switchMap(paused => paused ? never() : numbers$),
+      map(s => ({ hh: format(s / 3600), mm: format((s % 3600) / 60), ss: format(s % 60) })),
       takeUntil(this.stop$),
       finalize(() => this.running = false),
     );
@@ -38,6 +48,14 @@ export class AppComponent {
   stop() {
     /* document.mozCancelFullScreen(); */
     this.stop$.next();
+  }
+
+  pause() {
+    this.paused$.next(true);
+  }
+
+  continue() {
+    this.paused$.next(false);
   }
 
 }
